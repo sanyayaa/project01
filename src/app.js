81cf54deb06eb03a971f534637def2e842248ee4
Sanya <sanyayaatiwari@gmail.com>
2025-03-18 18:58:27 +0530

    routers n controller

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true,limit : "16kb"}))
 app.use(express.static("public"))
 
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+// dont use app.get lyuki we willl call through middleware coz our code is not uin a single file
+// http://localost:8000/users/register
+app.use("/api/v1/users",userRouter)
+
 export {app}
