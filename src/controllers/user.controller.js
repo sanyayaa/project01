3a7e4c881387950c2c30eb5d4469f80065d8e933
Sanya <sanyayaatiwari@gmail.com>
2025-03-22 23:05:36 +0530

    Aggregation Pipeline - getUserChannelProfile Controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -366,6 +366,82 @@ const updateUserCoverImage = asyncHandler(async(req,res) => {
     .json(new ApiResponse(200,user,"Cover Image Updated Successfully"))
 }) 
 
+const getUserChannelProfile = asyncHandler(async (req,res) => {
+    // we will not this data from our body but from parameter passed in url coz generally we go by routes
+    const {username} = req.params;
+    if(!username?.trim()){
+        throw new ApiError(400,"username is missing")
+    }
+    // aggregation pipeline
+    const channel = await User.aggregate([
+        {
+            $match : {
+                username : username?.toLowerCase()
+            }
+        },
+        {
+            $lookup : {
+                // name of table stored in db model mei Subscription hai lekin jab vo db mei save hota hai toh pplural and lowercase mei ho jata hai 
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "channel",
+                as : "subscribers"
+            }
+        },
+        {
+            $lookup : { 
+                from : "subscriptions",
+                localField : "_id",
+                foreignField : "subscriber",
+                as : "subscribedTo"
+            }
+        },
+        {
+            $addFields : {
+                subscribersCount : {
+                    // this will count from subscriber field we added above
+                    $size : "subscribers"
+                },
+                channelsSubscribedToCount : {
+                    // this will count how many one channels one has subscribed
+                    $size : "subscribedTo"
+                },
+                isSubscribed : {
+                    $cond : {
+                        if : {
+                            $in : [req.user?._id,"$subscribers.subscriber"]
+                        },
+                        then : true,
+                        else : false
+                    }
+                }
+            }
+        },
+        {   // what i want to send 
+            $project : {
+                fullName : 1,
+                username : 1,
+                subscribersCount : 1,
+                channelsSubscribedToCount : 1,
+                isSubscribed : 1,
+                avatar : 1,
+                coverImage : 1,
+                email : 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404,"Channel doesnot exists")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,channel[0],"User Channel fetched successfully")
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -374,5 +450,6 @@ export {
     changeCurrentPassword,
     getCurrentUser,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
